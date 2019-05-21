65190154060354fe1a4245fe108220b5c4e621b0
Tra Claude <vroclaude@gmail.com>
2019-05-21 03:20:25 +0200

    Creating Project

---
+++ src/main/java/com/clody/springboot/coursmc/CoursmcApplication.java
@@ -0,0 +1,13 @@
+package com.clody.springboot.coursmc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CoursmcApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CoursmcApplication.class, args);
+	}
+
+}

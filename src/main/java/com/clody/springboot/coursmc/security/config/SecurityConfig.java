0ac33a1ea859719ad8b59a9200aedd761d878765
Tra Claude <vroclaude@gmail.com>
2019-06-18 12:11:46 +0200

    Initial configuration of Spring Security

---
+++ src/main/java/com/clody/springboot/coursmc/security/config/SecurityConfig.java
@@ -0,0 +1,46 @@
+package com.clody.springboot.coursmc.security.config;
+
+import java.util.Arrays;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.env.Environment;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+	
+	@Autowired
+	private Environment environment;
+	
+	
+	 
+	private static final String[] PUBLIC_MATCHERS = { "/api/products/**", "/api/categories/**" };
+
+	@Override
+	protected void configure(HttpSecurity httpSecurity) throws Exception {
+		if(Arrays.asList(environment.getActiveProfiles()).contains("test")){
+			httpSecurity.headers().frameOptions().disable();
+		}
+		httpSecurity.cors().and().csrf().disable();
+		httpSecurity.authorizeRequests()
+		.antMatchers(PUBLIC_MATCHERS).permitAll()
+		.anyRequest().authenticated();;
+		httpSecurity.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
+	}
+
+	@Bean
+	CorsConfigurationSource corsConfigurationSource() {
+		final UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+		source.registerCorsConfiguration("/**", new CorsConfiguration().applyPermitDefaultValues());
+		return source;
+	}
+}
